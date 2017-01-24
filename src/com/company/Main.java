4794d953bd37266593f677a88e1d67308202ab49
Dr. Auxin <paul.torrillo@gmail.com>
2017-01-24 10:25:11 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+        public static void main( String args[])
+        { Picture pix = new Picture("beach.jpg");
+        } pix.explore();
+
+    }
+}
